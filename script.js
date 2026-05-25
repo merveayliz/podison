e085c036bffe00281a4cc09722abc4be1d193435
ayliz <merveayliz2301@gmail.com>
2026-05-25 12:18:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// --- PREMIUM GİRİŞ ANİMASYONU MOTORU ---
+
 window.addEventListener('DOMContentLoaded', () => {
-    // Sayfa tamamen yüklendikten 2 saniye sonra splash ekranı yukarı kayarak kaybolur
     setTimeout(() => {
         const splash = document.getElementById('splash-screen');
         if (splash) {
